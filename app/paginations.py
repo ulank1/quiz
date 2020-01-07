367ca9aee960f6e6b55d43354c155b91be3ea102
ulank1 <ulankarimov@mail.ru>
2020-01-07 15:07:03 +0600

    assd

--- app/paginations.py
+++ app/paginations.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 
 class LargeResultsSetPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 20
 
     def get_paginated_response(self, data):
         return Response({
