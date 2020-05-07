393c79102ba8082836cd0ee8a63c898c79e52695
ulank1 <ulankarimov@mail.ru>
2020-05-07 20:10:52 +0600

    asas

--- app/models.py
+++ app/models.py
@@ -40,7 +40,8 @@ class Users(models.Model):
 
     # rating = models.IntegerField(max_length=100, null=True, blank=True, verbose_name='рейтинг')
     def __str__(self):
-        return self.name
+        return str(self.login)
+
 
 class Test(models.Model):
     class Meta:
